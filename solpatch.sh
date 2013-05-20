20910f20ef331ed4228340e6e87068a1026d2eb5
BCWilsonDotCom <bwilson@me.com>
2013-05-20 17:39:10 -0600

    Update solpatch.sh
    change in header comment.

--- solpatch.sh
+++ solpatch.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# Script to automate the Solaris patching process at Hotwire
+# Script to automate the Solaris patching process
 #############################################################
 
 ############################################
